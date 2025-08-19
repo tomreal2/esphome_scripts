d8f2ce95f916adb4256cbff7211f41f82f079f84
Thomas Cowan <tom@ssdllc.biz>
2025-08-19 11:38:46 -0400

    moved non-specific stuff to a folder

--- capture-image-T.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if ping -c 1 192.168.1.213 &> /dev/null
-then
-  sleep 10
-	ffmpeg -rtsp_transport tcp -i rtsp://admin:Havea6and3@192.168.1.213:554/h264Preview_01_main -frames:v 1 /home/pi/blue/imgs/T/img-T-$(date +'%Y%m%d-%H%M%S').jpg
-fi
